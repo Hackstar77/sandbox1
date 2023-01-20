5c3b95368322559f0c6442d1457cf0f4c913aba3
Hackstar77 <contactmehereport3@gmail.com>
2023-01-20 19:53:33 +0100

    Adjusted Org. Regain for low reliability.

--- common/defines/elwolfmod_defines.lua
+++ common/defines/elwolfmod_defines.lua
@@ -17,6 +17,7 @@ NDefines.NMilitary.PROMOTE_LEADER_CP_COST = 0.11
 NDefines.NMilitary.RELIABILTY_RECOVERY = 0.0 -- WAS 0.1 factor affecting how much equipment is returned "from the dead"
 NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 2 -- WAS 1 aka TRAINED | Since the above was changed there is no point to not allowing divs to be trained to regular considering that its only 10% stats now.
 NDefines.NMilitary.UNIT_LEADER_ASSIGN_TRAIT_COST = 0.1
+NDefines.NMilitary.RELIABILITY_ORG_REGAIN = -0.42,                 -- how much reliability affects org regain
 NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 30                           -- Up from 10, should allow for more flexibility with picking focuses while doing something else, like tank templates
 NDefines.NAI.GIVE_STATE_CONTROL_MIN_CONTROLLED = 0
 NDefines.NAI.GIVE_STATE_CONTROL_MIN_CONTROL_DIFF = 0
